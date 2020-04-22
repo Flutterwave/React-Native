3d6e5840521a0b3eb731fb34a815deaa73698d8c
Daniel Barde <mrbarde2k@gmail.com>
2020-04-22 15:12:06 +0100

    test(__test__/customproptypesrule): test PaymentOptionsPropTypesRule
    Ensure when payment options are passed, it is a string that contains the valid payment options.

---
+++ __tests__/CustomPropTypesRules.spec.ts
@@ -0,0 +1,27 @@
+import 'react-native';
+import {PaymentOptionsPropRule} from '../src/utils/CustomPropTypesRules';
+const PropName = 'payment_options';
+
+describe('CustomPropTypes.PaymentOptionsPropRule', () => {
+  it ('returns null if prop is not defined in props', () => {
+    const result = PaymentOptionsPropRule({}, PropName);
+    expect(result).toBe(null);
+  });
+
+  it ('returns error if prop is not a string', () => {
+    const result = PaymentOptionsPropRule({[PropName]: []}, PropName);
+    expect(result !== null).toBe(true);
+    expect(result.message).toContain('should be a string.');
+  });
+
+  it ('returns error if prop includes invalid payment option', () => {
+    const result = PaymentOptionsPropRule({[PropName]: 'barter, foo'}, PropName);
+    expect(result !== null).toBe(true);
+    expect(result.message).toContain('must be any of the following values.');
+  });
+
+  it ('returns null if payment options are valid', () => {
+    const result = PaymentOptionsPropRule({[PropName]: 'barter'}, PropName);
+    expect(result).toBe(null);
+  });
+})
