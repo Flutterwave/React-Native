185ea28368ecc0ab8ab4498fb1e09101ed520787
Daniel Barde <mrbarde2k@gmail.com>
2020-07-15 16:54:41 +0100

    feat(responseparser): promisify; resolve to link else reject to FlutterwaveInitError

--- src/utils/ResponseParser.ts
+++ src/utils/ResponseParser.ts
@@ -1,8 +1,6 @@
 import { ResponseData } from "../FlutterwaveInit";
 import FlutterwaveInitError from "./FlutterwaveInitError";
 
-type ParsedSuccess = string;
-
 /**
  * The purpose of this function is to parse the response message gotten from a
  * payment initialization error.
@@ -19,45 +17,49 @@ export default function ResponseParser(
     code,
     error_id,
   }: ResponseData
-): FlutterwaveInitError | ParsedSuccess {
-  // return success message
-  if (status === 'success') {
-    // check if data or data link is missing
-    if (!data || !data.link) {
-      return new FlutterwaveInitError({
-        code: 'MALFORMED_RESPONSE',
-        message,
-      })
+): Promise<string> {
+  return new Promise((resolve, reject) => {
+    // return success message
+    if (status === 'success') {
+      // check if data or data link is missing
+      if (!data || !data.link) {
+        return reject(new FlutterwaveInitError({
+          code: 'MALFORMED_RESPONSE',
+          message,
+        }))
+      }
+      // return the payment link
+      return resolve(data.link);
     }
-    // return the payment link
-    return data.link;
-  }
-  // missing authorization
-  if (/authorization/i.test(message) && /required/i.test(message)) {
-    return new FlutterwaveInitError({
-      code: 'AUTH_MISSING',
+    // missing authorization
+    if (/authorization/i.test(message) && /required/i.test(message)) {
+      reject(
+        new FlutterwaveInitError({
+          code: 'AUTH_MISSING',
+          message,
+        })
+      );
+    }
+    // invalid authorization
+    if (/authorization/i.test(message) && /invalid/i.test(message)) {
+      reject(new FlutterwaveInitError({
+        code: 'AUTH_INVALID',
+        message,
+      }));
+    }
+    // field errors
+    if (errors) {
+      reject(new FlutterwaveInitError({
+        code: 'INVALID_OPTIONS',
+        message: message,
+        errors: errors.map(i => i.message),
+      }));
+    }
+    // defaults to the initially passed message
+    reject(new FlutterwaveInitError({
+      code: String(code || 'STANDARD_INIT_ERROR').toUpperCase(),
       message,
-    });
-  }
-  // invalid authorization
-  if (/authorization/i.test(message) && /invalid/i.test(message)) {
-    return new FlutterwaveInitError({
-      code: 'AUTH_INVALID',
-      message,
-    });
-  }
-  // field errors
-  if (errors) {
-    return new FlutterwaveInitError({
-      code: 'INVALID_OPTIONS',
-      message,
-      errors: errors.map(i => i.message),
-    });
-  }
-  // defaults to the initially passed message
-  return new FlutterwaveInitError({
-    code: String(code || 'STANDARD_INIT_ERROR').toUpperCase(),
-    message,
-    errorId: error_id
-  });
+      errorId: error_id
+    }));
+  })
 }
