a851b8645973c64773328697f7f2e38633e13034
Daniel Barde <mrbarde2k@gmail.com>
2020-07-15 16:56:19 +0100

    test(responseparser): rewrite test to use as a promise

--- __tests__/ResponseParser.spec.ts
+++ __tests__/ResponseParser.spec.ts
@@ -2,76 +2,101 @@ import ResponseParser from '../src/utils/ResponseParser';
 import FlutterwaveInitError from '../src/utils/FlutterwaveInitError';
 
 describe('<ResponseParser/>', () => {
-  it('returns missing auth error', () => {
-    const code = 'AUTH_MISSING';
-    // parse response
-    const error = ResponseParser({
-      message: 'Authorization is required'
-    });
-    // run assertions
-    expect(error instanceof FlutterwaveInitError).toBeTruthy()
-    expect(typeof error !== 'string' && error.code === code).toBeTruthy();
+  it('expect error to be instance of FlutterwaveInitError', async () => {
+    try {
+      // parse response
+      await ResponseParser({
+        message: 'Error occurred!'
+      });
+    } catch (error) {
+      // run assertions
+      expect(error).toBeInstanceOf(FlutterwaveInitError);
+    }
+  });
+  
+  it('returns missing auth error', async () => {
+    try {
+      // parse response
+      await ResponseParser({
+        message: 'Authorization is required'
+      });
+    } catch (error) {
+      // run assertions
+      expect(error).toBeInstanceOf(FlutterwaveInitError);
+      expect(error.code).toEqual('AUTH_MISSING');
+    }
   });
 
-  it('returns invalid auth error', () => {
-    const code = 'AUTH_INVALID';
-    // parse response
-    const error = ResponseParser({
-      message: 'Authorization is invalid'
-    });
-    // run assertions
-    expect(error instanceof FlutterwaveInitError).toBeTruthy()
-    expect(typeof error !== 'string' && error.code === code).toBeTruthy();
+  it('returns invalid auth error', async () => {
+    try {
+      // parse response
+      await ResponseParser({
+        message: 'Authorization is invalid'
+      });
+    } catch (error) {
+      // run assertions
+      expect(error).toBeInstanceOf(FlutterwaveInitError);
+      expect(error.code).toEqual('AUTH_INVALID');
+    }
   });
 
-  it('return object with errors property', () => {
-    const code = 'INVALID_OPTIONS';
+  it('return object with errors property', async () => {
     const errors = [{field: 'tx_ref', message: 'An error has occured'}];
-    // parse response
-    const error = ResponseParser({
-      message: 'Missing fields error.',
-      errors
-    });
-    // run assertions
-    expect(error instanceof FlutterwaveInitError).toBeTruthy()
-    expect(typeof error !== 'string' && error.code === code).toBeTruthy();
-    expect(typeof error !== 'string' && Array.isArray(error.errors)).toBeTruthy();
-    expect(typeof error !== 'string' && error.errors[0] === errors[0].message).toBeTruthy();
+    try {
+      // parse response
+      await ResponseParser({
+        message: 'Missing fields error.',
+        errors
+      });
+    } catch (error) {
+      // run assertions
+      expect(error).toBeInstanceOf(FlutterwaveInitError);
+      expect(error.code).toEqual('INVALID_OPTIONS');
+      expect(Array.isArray(error.errors)).toBeTruthy();
+      expect(error.errors[0]).toEqual(errors[0].message);
+    }
   });
 
-  it('returns standard init error code as default code if none is specified', () => {
-    const code = 'STANDARD_INIT_ERROR';
-    // parse response
-    const error = ResponseParser({
-      message: 'A server error occurred.'
-    });
-    // run assertions
-    expect(error instanceof FlutterwaveInitError).toBeTruthy()
-    expect(typeof error !== 'string' && error.code === code).toBeTruthy();
+  it('returns standard init error code as default code if none is specified', async () => {
+    try {
+      // parse response
+      ResponseParser({
+        message: 'A server error occurred.'
+      });
+    } catch (error) {
+      // run assertions
+      expect(error).toBeInstanceOf(FlutterwaveInitError);
+      expect(error.code).toEqual('STANDARD_INIT_ERROR');
+    }
   });
 
-  it('returns malformed response if status is success and data or link is missing', () => {
-    const code = 'MALFORMED_RESPONSE';
-    // parse response
-    const error = ResponseParser({
-      message: 'Great, it worked!!!',
-      status: 'success'
-    });
-    // run assertions
-    expect(error instanceof FlutterwaveInitError).toBeTruthy()
-    expect(typeof error !== 'string' && error.code === code).toBeTruthy();
+  it('returns malformed response if status is success and data or link is missing', async () => {
+    try {
+      // parse response
+      await ResponseParser({
+        message: 'Great, it worked!!!',
+        status: 'success'
+      });
+    } catch (error) {
+      // run assertions
+      expect(error).toBeInstanceOf(FlutterwaveInitError);
+      expect(error.code).toEqual('MALFORMED_RESPONSE');
+    }
   });
 
-  it('returns a payment link', () => {
-    const link = 'http://checkout.flutterwave.com/380340934u093u403'
-    // parse response
-    const data = ResponseParser({
-      message: 'Hosted link.',
-      status: 'success',
-      data: {link}
-    });
-    // run assertions
-    expect(data instanceof FlutterwaveInitError).toBeFalsy()
-    expect(data === link).toBeTruthy();
+  it('returns a payment link', async () => {
+    try {
+      const link = 'http://checkout.flutterwave.com/380340934u093u403'
+      // parse response
+      const data = await ResponseParser({
+        message: 'Hosted link.',
+        status: 'success',
+        data: {link}
+      });
+      // run assertions
+      expect(data).toEqual(link);
+    } catch (err) {
+      // no need to run this
+    }
   });
 });
