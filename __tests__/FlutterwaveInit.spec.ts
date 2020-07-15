d8dfedb8f2cd2bee56e870c33ec25dc3a11832f8
Daniel Barde <mrbarde2k@gmail.com>
2020-07-15 17:09:53 +0100

    test(flutterwaveinit): refactor test suite and handle resolve and reject calls

--- __tests__/FlutterwaveInit.spec.ts
+++ __tests__/FlutterwaveInit.spec.ts
@@ -1,117 +1,172 @@
 import FlutterwaveInit, {FlutterwaveInitOptions} from '../src/FlutterwaveInit';
 import {STANDARD_URL} from '../src/configs';
+import FlutterwaveInitError from '../src/utils/FlutterwaveInitError';
 
-// default fetch header
-const DefaultFetchHeader = new Headers();
-DefaultFetchHeader.append('Content-Type', 'application/json');
+const AUTHORIZATION = '[AUTHORIZATION]';
+
+// fetch header
+const FETCH_HEADER = new Headers();
+FETCH_HEADER.append('Content-Type', 'application/json');
+FETCH_HEADER.append('Authorization', `Bearer ${AUTHORIZATION}`);
+
+// fetch body
+const FETCH_BODY = {
+  redirect_url: 'http://flutterwave.com',
+  amount: 50,
+  currency: 'NGN',
+  customer: {
+    email: 'email@example.com',
+  },
+  tx_ref: Date.now() + '-txref',
+  payment_options: 'card',
+  customizations: {
+    title: 'Hello World',
+  }
+}
+
+// payment options
+const INIT_OPTIONS: FlutterwaveInitOptions = {
+  ...FETCH_BODY,
+  authorization: AUTHORIZATION
+};
 
 describe('<FlutterwaveInit />', () => {
-  it('returns a payment link after initialization', async () => {
+  it('runs a fetch request', async () => {
     // mock next fetch request
     fetchMock.mockOnce(JSON.stringify({
       status: 'success',
       message: 'Payment link generated.',
-      data: {
-        link: 'http://payment-link.com/checkout',
-      },
+      data: {link: 'http://example.com'},
     }));
-    // payment information
-    const paymentInfo: FlutterwaveInitOptions = {
-      redirect_url: 'http://flutterwave.com',
-      PBFPubKey: '[PUB Key]',
-      amount: 50,
-      currency: 'NGN',
-      customer_email: 'email@example.com',
-      txref: Date.now() + '-txref',
-    };
-    // flutterwave init test
-    const response = await FlutterwaveInit(paymentInfo);
-    
-    // expect fetch to have been called once
-    expect(global.fetch).toHaveBeenCalledTimes(1);
-    // expect fetch to have been called to the standard init url
-    expect(global.fetch).toHaveBeenCalledWith(STANDARD_URL, {
-      body: JSON.stringify(paymentInfo),
-      headers: DefaultFetchHeader,
-      method: 'POST',
-    });
-    expect(typeof response.link === 'string').toBeTruthy();
+    try {
+      // initialize payment
+      await FlutterwaveInit(INIT_OPTIONS);
+      // run assertions
+      expect(global.fetch).toHaveBeenCalledTimes(1);
+      expect(global.fetch).toHaveBeenCalledWith(STANDARD_URL, {
+        body: JSON.stringify(FETCH_BODY),
+        headers: FETCH_HEADER,
+        method: 'POST',
+      });
+    } catch (e) {
+      // no error occurred
+    }
+  });
+
+  it('returns a payment link after initialization', async () => {
+    const link = 'http://payment-link.com/checkout';
+    // mock next fetch request
+    fetchMock.mockOnce(JSON.stringify({
+      status: 'success',
+      message: 'Payment link generated.',
+      data: {link},
+    }));
+    try {
+      // initialize payment
+      const response = await FlutterwaveInit(INIT_OPTIONS)
+      // run assertions
+      expect(response).toEqual(link)
+    } catch (e) {
+      // no error occurred
+    }
   });
 
   it('includes error code and message of init error', async () => {
-    // payment information
-    const paymentInfo: FlutterwaveInitOptions = {
-      redirect_url: 'http://flutterwave.com',
-      PBFPubKey: 'FLWPUBK_TEST-c761fb7f0e443f5704a796781b621875-X44',
-      amount: 50,
-      currency: 'NGN',
-      customer_email: 'email@example.com',
-      txref: Date.now() + '-txref',
-    };
+    const message = 'An error has occurred.';
     // reject next fetch
-    fetchMock.mockRejectOnce(new Error('An error occured!'));
-    // flutterwave init test
-    const response = await FlutterwaveInit(paymentInfo);
-    // expect fetch to have been called
-    expect(global.fetch).toHaveBeenCalledTimes(1);
-    expect(global.fetch).toHaveBeenCalledWith(STANDARD_URL, {
-      body: JSON.stringify(paymentInfo),
-      headers: DefaultFetchHeader,
-      method: 'POST',
-    });
-    // expect error and error code to be defined
-    expect(typeof response.error.code === 'string').toBeTruthy();
-    expect(typeof response.error.message === 'string').toBeTruthy();
+    fetchMock.mockOnce(JSON.stringify({
+      status: 'error',
+      message: message,
+    }));
+    try {
+      // initialize payment
+      await FlutterwaveInit(INIT_OPTIONS);
+    } catch (error) {
+      // run assertions
+      expect(error.message).toEqual(message);
+      expect(error.code).toEqual('STANDARD_INIT_ERROR');
+    }
   });
 
-  it('returns unknown error if the error response has no code or message', async () => {
-    // payment information
-    const paymentInfo: FlutterwaveInitOptions = {
-      redirect_url: 'http://flutterwave.com',
-      PBFPubKey: 'FLWPUBK_TEST-c761fb7f0e443f5704a796781b621875-X44',
-      amount: 50,
-      currency: 'NGN',
-      customer_email: 'email@example.com',
-      txref: Date.now() + '-txref',
-    };
-    // mock next fetch
-    fetchMock.mockOnce(JSON.stringify({status: 'error', data: {}}));
-    // flutterwave init test
-    const response = await FlutterwaveInit(paymentInfo);
-    // expect fetch to have been called
-    expect(global.fetch).toHaveBeenCalledTimes(1);
-    expect(global.fetch).toHaveBeenCalledWith(STANDARD_URL, {
-      body: JSON.stringify(paymentInfo),
-      headers: DefaultFetchHeader,
-      method: 'POST',
-    });
-    // expect unkown error from from response
-    expect(/unknown/i.test(response.error.code)).toBeTruthy();
+  it('returns missing authorization request error code', async () => {
+    const message = 'Authorization is required.';
+    // reject next fetch
+    fetchMock.mockOnce(JSON.stringify({
+      status: 'error',
+      message: message,
+    }));
+    try {
+      // initialize payment
+      await FlutterwaveInit(INIT_OPTIONS);
+    } catch (error) {
+      // run assertions
+      expect(error.message).toEqual(message);
+      expect(error.code).toEqual('AUTH_MISSING');
+    }
+  });
+
+  it('returns invalid authorization request error code', async () => {
+    const message = 'Authorization is invalid.';
+    // reject next fetch
+    fetchMock.mockOnce(JSON.stringify({
+      status: 'error',
+      message: message,
+    }));
+    try {
+      // initialize payment
+      await FlutterwaveInit(INIT_OPTIONS);
+    } catch (error) {
+      // run assertions
+      expect(error.message).toEqual(message);
+      expect(error.code).toEqual('AUTH_INVALID');
+    }
+  });
+
+  it('returns field errors from request errors', async () => {
+    const message = 'Missin fields.';
+    const errors = [{field: 'tx_ref', message: 'tx_ref is required'}];
+    // reject next fetch
+    fetchMock.mockOnce(JSON.stringify({
+      status: 'error',
+      message: message,
+      errors: errors,
+    }));
+    try {
+      // initialize payment
+      await FlutterwaveInit(INIT_OPTIONS);
+    } catch (error) {
+      // run assertions
+      expect(error.message).toEqual(message);
+      expect(error.code).toEqual('INVALID_OPTIONS');
+      expect(error.errors).toBeDefined();
+      expect(error.errors[0]).toEqual(errors[0].message);
+    }
+  });
+
+  it('always has an error code property on error object', async () => {
+    const message = 'An error has occurred.';
+    // reject next fetch
+    fetchMock.mockRejectOnce(new Error(message));
+    try {
+      // initialize payment
+      await FlutterwaveInit(INIT_OPTIONS);
+    } catch (error) {
+      // run assertions
+      expect(error.code).toBeDefined();
+    }
   });
 
   it('catches missing response data error', async () => {
-    // payment information
-    const paymentInfo: FlutterwaveInitOptions = {
-      redirect_url: 'http://flutterwave.com',
-      PBFPubKey: 'FLWPUBK_TEST-c761fb7f0e443f5704a796781b621875-X44',
-      amount: 50,
-      currency: 'NGN',
-      customer_email: 'email@example.com',
-      txref: Date.now() + '-txref',
-    };
+    const message = 'Hello, World!';
     // mock next fetch
-    fetchMock.mockOnce(JSON.stringify({status: 'error'}));
-    // flutterwave init test
-    const response = await FlutterwaveInit(paymentInfo);
-    // expect fetch to have been called
-    expect(global.fetch).toHaveBeenCalledTimes(1);
-    expect(global.fetch).toHaveBeenCalledWith(STANDARD_URL, {
-      body: JSON.stringify(paymentInfo),
-      headers: DefaultFetchHeader,
-      method: 'POST',
-    });
-    // expect a no response error
-    expect(/nodata/i.test(response.error.code)).toBeTruthy();
+    fetchMock.mockOnce(JSON.stringify({status: 'success', message}));
+    try {
+      // initialize payment
+      await FlutterwaveInit(INIT_OPTIONS);
+    } catch (error) {
+      // run assertions
+      expect(error.code).toEqual('MALFORMED_RESPONSE');
+    }
   });
 
   it('is abortable', async () => {
@@ -120,26 +175,25 @@ describe('<FlutterwaveInit />', () => {
     // mock fetch response
     fetchMock.mockResponse(async () => {
       jest.advanceTimersByTime(60)
-      return ''
+      return JSON.stringify({
+        status: 'error',
+        message: 'Error!',
+      })
     });
     // create abort controller
     const abortController = new AbortController;
-    // payment information
-    const paymentInfo: FlutterwaveInitOptions = {
-      redirect_url: 'http://flutterwave.com',
-      PBFPubKey: 'FLWPUBK_TEST-c761fb7f0e443f5704a796781b621875-X44',
-      amount: 50,
-      currency: 'NGN',
-      customer_email: 'email@example.com',
-      txref: Date.now() + '-txref',
-    };
     // abort next fetch
     setTimeout(() => abortController.abort(), 50);
-    // expect a no response error
-    await expect(FlutterwaveInit(paymentInfo, {canceller: abortController})).resolves.toMatchObject({
-      error: {
-        code: 'ABORTERROR'
-      }
-    });
+    try {
+      // initialize payment
+      await FlutterwaveInit(
+        INIT_OPTIONS,
+        abortController
+      )
+    } catch(error) {
+      // run assertions
+      expect(error).toBeInstanceOf(FlutterwaveInitError);
+      expect(error.code).toEqual('ABORTERROR');
+    }
   });
 });
