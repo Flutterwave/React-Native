495ac216eaf0bc76a18d6c9534200d770ad4bf83
Daniel Barde <mrbarde2k@gmail.com>
2020-07-21 15:25:04 +0100

    feat(index): add v2 to base index modules export

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import FlutterwaveInit from './FlutterwaveInit';
 import FlutterwaveButton from './FlutterwaveButton';
+import FlutterwaveInitV2 from './v2/FlutterwaveInit';
+import FlutterwaveButtonV2 from './v2/FlutterwaveButton';
 import DefaultButton from './DefaultButton';
 
 // export modules
-export {FlutterwaveInit, FlutterwaveButton, DefaultButton};
+export {
+  FlutterwaveInit,
+  FlutterwaveButton,
+  FlutterwaveInitV2,
+  FlutterwaveButtonV2,
+  DefaultButton,
+};
 
-// export init as default
+// export v3 button as default
 export default FlutterwaveButton;
