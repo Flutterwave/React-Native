d8bbb745bb27b3568b8ce060b6670fbdfb73218c
Daniel Barde <mrbarde2k@gmail.com>
2020-07-21 14:41:47 +0100

    feat(configs): add v2 api configs

--- src/configs.ts
+++ src/configs.ts
@@ -1,16 +1,25 @@
 /**
- * Flutterwaves standard init url.
+ * V# API Standard initialization endpoint
  */
 export const STANDARD_URL = 'https://api.flutterwave.com/v3/payments';
 
+/**
+ * Redirect URL used in V3 FlutterwaveButton
+ */
 export const REDIRECT_URL = 'https://flutterwave.com/rn-redirect';
 
+/**
+ * Fluttereave volors
+ */
 export const colors = {
   primary: '#f5a623',
   secondary: '#12122C',
   transparent: 'rgba(0,0,0,0)',
 };
 
+/**
+ * Payment options available in V3
+ */
 export const PAYMENT_OPTIONS = [
   'account',
   'card',
@@ -29,4 +38,29 @@ export const PAYMENT_OPTIONS = [
   'paga',
   '1voucher',
   'mobilemoneytanzania',
+];
+
+/**
+ * V2 API standard initialization endpoint
+ */
+export const STANDARD_URL_V2: string =
+  'https://api.ravepay.co/flwv3-pug/getpaidx/api/v2/hosted/pay';
+
+/**
+ * Payment options available in V2 API
+ */
+export const PAYMENT_OPTIONS_V2 = [
+  'card',
+  'account',
+  'ussd',
+  'qr',
+  'mpesa',
+  'mobilemoneyghana',
+  'mobilemoneyuganda',
+  'mobilemoneyrwanda',
+  'mobilemoneyzambia',
+  'mobilemoneytanzania',
+  'barter',
+  'bank transfer',
+  'wechat',
 ];
