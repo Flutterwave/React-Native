b285ec95e2e183fa4fece50a1ffafcd68bee8ce3
Daniel Barde <mrbarde2k@gmail.com>
2020-07-21 15:22:00 +0100

    feat(v2/index): add default button to exports

--- src/v2/index.ts
+++ src/v2/index.ts
@@ -1,8 +1,9 @@
 import FlutterwaveInit from './FlutterwaveInit';
 import FlutterwaveButton from './FlutterwaveButton';
+import DefaultButton from '../DefaultButton';
 
 // export modules
-export {FlutterwaveInit, FlutterwaveButton};
+export {FlutterwaveInit, FlutterwaveButton, DefaultButton};
 
 // export init as default
 export default FlutterwaveButton;
