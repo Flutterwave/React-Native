e1a0825260d50752c360c2cc26dd7ac9d54052ca
Daniel Barde <mrbarde2k@gmail.com>
2020-08-03 09:47:44 +0100

    feat(index.ts): add PayWithFlutterwave component and make the default export

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import FlutterwaveButton from './FlutterwaveButton';
 import FlutterwaveButtonV2 from './v2/FlutterwaveButton';
 import DefaultButton from './DefaultButton';
 import FlutterwaveInitV2 from './FlutterwaveInitV2';
+import PayWithFlutterwave from './PayWithFlutterwave';
 import FlwButton from './FlwButton';
 import FlwCheckout from './FlwCheckout';
 
@@ -10,6 +11,7 @@ import FlwCheckout from './FlwCheckout';
 export {
   FlutterwaveInit,
   FlutterwaveButton,
+  PayWithFlutterwave,
   FlutterwaveInitV2,
   FlutterwaveButtonV2,
   DefaultButton,
@@ -17,5 +19,5 @@ export {
   FlwCheckout,
 };
 
-// export v3 button as default
-export default FlutterwaveButton;
+// export v3 PayWithFlutterwave as default
+export default PayWithFlutterwave;
