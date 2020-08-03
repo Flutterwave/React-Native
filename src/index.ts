4cf13f9bdd71dee6847be1d90bff4dc912e1d651
Daniel Barde <mrbarde2k@gmail.com>
2020-08-03 10:01:35 +0100

    refactor: remove old immplementations code

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
 import FlutterwaveInit from './FlutterwaveInit';
-import FlutterwaveButton from './FlutterwaveButton';
-import FlutterwaveButtonV2 from './v2/FlutterwaveButton';
-import DefaultButton from './DefaultButton';
 import FlutterwaveInitV2 from './FlutterwaveInitV2';
 import PayWithFlutterwave from './PayWithFlutterwave';
 import PayWithFlutterwaveV2 from './PayWithFlutterwaveV2';
@@ -11,11 +8,8 @@ import FlwCheckout from './FlwCheckout';
 // export modules
 export {
   FlutterwaveInit,
-  FlutterwaveButton,
   PayWithFlutterwave,
   FlutterwaveInitV2,
-  FlutterwaveButtonV2,
-  DefaultButton,
   PayWithFlutterwaveV2,
   FlwButton,
   FlwCheckout,
--- src/v2/index.ts
+++
@@ -1,9 +0,0 @@
-import FlutterwaveInit from './FlutterwaveInit';
-import FlutterwaveButton from './FlutterwaveButton';
-import DefaultButton from '../DefaultButton';
-
-// export modules
-export {FlutterwaveInit, FlutterwaveButton, DefaultButton};
-
-// export button as default
-export default FlutterwaveButton;
