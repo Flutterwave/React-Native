6c7307a55109af067637e64c7f7585523f73d32c
Daniel Barde <mrbarde2k@gmail.com>
2020-08-18 10:34:46 +0100

    refactor: remove dist folder

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import FlutterwaveInit from './FlutterwaveInit';
-import FlutterwaveInitV2 from './FlutterwaveInitV2';
-import PayWithFlutterwave from './PayWithFlutterwave';
-import PayWithFlutterwaveV2 from './PayWithFlutterwaveV2';
-import FlutterwaveButton from './FlutterwaveButton';
-import FlutterwaveCheckout from './FlutterwaveCheckout';
-export { FlutterwaveInit, PayWithFlutterwave, FlutterwaveInitV2, PayWithFlutterwaveV2, FlutterwaveButton, FlutterwaveCheckout, };
-export default PayWithFlutterwave;
-//# sourceMappingURL=index.d.ts.map
--- dist/utils/CustomPropTypesRules.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const PaymentOptionsPropRule: (options: string[]) => (props: {
-    [k: string]: any;
-}, propName: string) => Error | null;
-//# sourceMappingURL=CustomPropTypesRules.d.ts.map
